c04e3131e9998427cd634658cbc6cd1ecac9ae76
Charlie Greenman <charie@razroo.com>
2016-08-08 22:54:29 -0400

    Revert "Remove routing in app.routes.ts now that routing has essentially been removed."
    This reverts commit 454f040793f64b9191657c213ed44fee2260b6e8.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,6 @@ import { XLarge } from './x-large';
   // for `document.querySelectorAll(selector)` in our index.html
   // where, in this case, selector is the string 'home'
   selector: 'home',  // <home></home>
-  template: '',
   // We need to tell Angular's Dependency Injection which providers are in our app.
   providers: [
     Title
